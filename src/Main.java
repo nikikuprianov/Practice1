2339ad78a790f2fe6e0b9bd7de15be38e9bc8a34
Abrams03_ <nikitka.makarov.2003@mail.ru>
2022-05-12 23:24:33 +0600

    commit first

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import fona.phone;
 public class Main {
     public static void main(String[] args) {
-        phone Phone1 = new phone(880055535, "Galaxy s4", "alex", 0.150);
-        phone Phone3 = new phone(228691337, "Galaxy s10");
+        phone Phone1 = new phone();
+        phone Phone2 = new phone(88005535, "Galaxy s7");
+        phone Phone3 = new phone(228691337, "Galaxy s10", "Вован", 0.220);
 
 
-        Phone1.reciveCall(Phone1.ownerName);
-        Phone1.reciveCall(Phone1.ownerName);
-        Phone1.reciveCall(Phone1.ownerName);
+        Phone1.reciveCall(Phone2.getOwnerName());
+        Phone2.reciveCall(Phone3.getOwnerName());
+        Phone3.reciveCall(Phone1.getOwnerName());
 
 
-        Phone1.receiveCall(Phone1.ownerName, Phone1.number);
+        Phone1.receiveCall(Phone1.getOwnerName(), Phone1.getNumber());
 
 
         System.out.println(Phone1.getNumber());;
--- src/fona/phone.java
+++ src/fona/phone.java
@@ -1,21 +1,51 @@
 package fona;
 public class phone {
-    public int number;
-    String model;
-    public String ownerName;
-    double weight;
-///////////////////////////////////////////////////////////
-    public phone() {
-    }
-    public phone(int number, String model, String ownerName, double weight) {
+    private int number;
+    private String model;
+    private String ownerName;
+    private double weight;
+
+
+    ///////////////////////////////////////////////////////////
+    public void setNumber(int number) {
         this.number = number;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
         this.model = model;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
         this.weight = weight;
     }
+/////////////////////////////////////////////////////////////
+    public phone() {
+        this.number = 4941414;
+        this.model = "Galaxy s4";
+        this.ownerName = "Зак";
+        this.weight = 0.160;
+    }
+
     public phone(int number, String model) {
         this.number = number;
         this.model = model;
+        this.ownerName = "Евгений";
+        this.weight = 0.228;
+    }
+    public phone(int number, String model, String ownerName, double weight) {
+        this(number, model);
+        this.ownerName = ownerName;
+        this.weight = weight;
     }
 //////////////////////////////////////////////////////////////////
     public void reciveCall(String ownerName) {
